d7ec84e1644c7b9a3c5b78c8bf4e004ceda45ce6
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-05-27 20:35:04 +0300

    Updates

---
+++ precache-manifest.b6f8becdbf300afe42049daf57c38614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d791637b7c4209cc60b4d30658fb86",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "82280000f71a03721239",
+    "url": "/react-calculator/static/css/main.12dcbc6c.chunk.css"
+  },
+  {
+    "revision": "2c4a14945eca8a41a510",
+    "url": "/react-calculator/static/js/2.3893bd6a.chunk.js"
+  },
+  {
+    "revision": "82280000f71a03721239",
+    "url": "/react-calculator/static/js/main.3cc127e1.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
